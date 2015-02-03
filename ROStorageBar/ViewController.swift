8d19efc769bc1949b03e6bc7b9b07c62621d23c0
Robin Oster <robin.oster@rascor.com>
2015-02-03 17:17:58 +0100

    Initial Commit

---
+++ ROStorageBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROStorageBar
+//
+//  Created by Robin Oster on 03/02/15.
+//  Copyright (c) 2015 Robin Oster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
